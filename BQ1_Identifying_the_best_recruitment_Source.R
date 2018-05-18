0b3c0f359b08d8c6f4d573eca995578a7175387d
Aditya Dhananjay Wagholikar <waditya02@gmail.com>
2018-05-18 17:30:31 -0500

    Update BQ1_Identifying_the_best_recruitment_Source.R

--- BQ1_Identifying_the_best_recruitment_Source.R
+++ BQ1_Identifying_the_best_recruitment_Source.R
@@ -6,3 +6,9 @@ recruitment <- read_csv("recruitment_data.csv")
 
 # Look at the first few rows of the dataset
 head(recruitment)
+
+##Identifying groups in data
+# Help the talent acquisition team understand which recruiting channel will produce the best sales hires. 
+#Let us apply the HR analytics process to help them. 
+#Let us examine the recruiting channels in the data.
+
