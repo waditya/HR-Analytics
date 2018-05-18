b28b8485f67079fa3758ab9ce49cc4fe8f3323c5
Aditya Dhananjay Wagholikar <waditya02@gmail.com>
2018-05-18 17:20:24 -0500

    Update BQ1_Identifying_the_best_recruitment_Source.R

--- BQ1_Identifying_the_best_recruitment_Source.R
+++ BQ1_Identifying_the_best_recruitment_Source.R
@@ -1,1 +1,8 @@
+# Load the readr package
+library(readr)
 
+# Import the recruitment data
+recruitment <- read_csv("recruitment_data.csv")
+
+# Look at the first few rows of the dataset
+head(recruitment)
